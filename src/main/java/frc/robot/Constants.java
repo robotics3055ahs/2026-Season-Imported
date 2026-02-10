aca6a522dc1ff0b04dc45e42d8bd44599a48278d
Furious George 3055 <robotics3055ahs@gmail.com>
2026-02-10 16:06:00 -0600

    No major change, preparing to make constants adjustable via smartdashboard

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -168,7 +168,7 @@ public final class Constants {
     public static final double VISION_STRAFE_kP = 0.3; // (m/s)
     public static final double VISION_STRAFE_kI = 0.0000;
     public static final double VISION_STRAFE_kD = 0.01;
-    public static final double VISION_STRAFE_OUTPUT_DEADBAND = ;
+    public static final double VISION_STRAFE_OUTPUT_DEADBAND = 0.005;
     // Camera constants
     public static final double cameraHeightMeters = 0.1063625; // 4.1825 inches
     //public static final double cameraYawSpeed = 0.1 * Math.PI; // similar to kMaxModuleAngularAccelerationRadiansPerSecondSquared
